b30c1fb907133c53ca9e16577c021bede5449fed
Tony Spiro <tspiro@tonyspiro.com>
2024-03-26 13:55:37 -0700

    edit: stripe / cosmic connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import { cosmicBucketConfig } from "@/lib/cosmic"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-
-const stripe = require("stripe")(process.env.STRIPE_API_KEY)
-
-function AddStripeProduct({ object }: { object?: any }) {
-  return (
-    <div>
-      <h1>Add {object.title} to Stripe</h1>
-      <Button>Add Product</Button>
-    </div>
-  )
-}
-
-async function ProductSection({ product }: { product: any }) {
-  const price = await stripe.prices.retrieve(product.default_price)
-  return (
-    <div className="space-y-4">
-      <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-        Edit {product.name}
-      </h1>
-      <div>
-        <Label htmlFor="title">Title</Label>
-        <Input
-          id="title"
-          type="text"
-          placeholder="Title"
-          defaultValue={product.name}
-        />
-      </div>
-      <div>
-        <Label>Images</Label>
-        <p className="text-lg mb-4 text-gray-800 dark:text-dark-gray-800">
-          <img
-            src={product.images[0]}
-            className="w-[100px] h-[100px] object-cover rounded-lg"
-          />
-        </p>
-      </div>
-      <div>
-        <Label htmlFor="price">Price</Label>
-        <Input
-          id="price"
-          type="number"
-          placeholder="Price"
-          defaultValue={price.unit_amount / 100}
-        />
-      </div>
-      <Button>Edit Product</Button>
-    </div>
-  )
-}
 
 export default async function IndexPage({
   searchParams,
@@ -62,6 +13,7 @@ export default async function IndexPage({
     write_key: string
     location: string
     object_id: string
+    stripe_secret_key: string
   }
 }) {
   const cosmic = cosmicBucketConfig(
@@ -69,16 +21,73 @@ export default async function IndexPage({
     searchParams.read_key,
     searchParams.write_key
   )
-  const cosmic_object_id = "65dba337919fff9f84c6ede7"
+  const cosmic_object_id = searchParams.object_id
+  const { object } = await cosmic.objects.findOne({
+    id: cosmic_object_id,
+  })
+  // Init Stripe
+  const stripe = require("stripe")(searchParams.stripe_secret_key)
   const { data } = await stripe.products.list()
   const product = data.filter(
     (prod: any) => prod?.metadata?.cosmic_object_id === cosmic_object_id
   )[0]
-  // See the Cosmic docs for more info https://www.cosmicjs.com/docs
+
+  function AddStripeProduct({ object }: { object?: any }) {
+    return (
+      <div>
+        <h1 className="mb-6">Add {object.title} to Stripe</h1>
+        <Button>Add Product</Button>
+      </div>
+    )
+  }
+
+  async function ProductSection({ product }: { product: any }) {
+    const price = await stripe.prices.retrieve(product.default_price)
+    return (
+      <div className="space-y-4">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Edit {product.name}
+        </h1>
+        <div>
+          <Label htmlFor="title">Title</Label>
+          <Input
+            id="title"
+            type="text"
+            placeholder="Title"
+            defaultValue={product.name}
+          />
+        </div>
+        <div>
+          <Label>Images</Label>
+          <p className="mb-4 text-lg text-gray-800 dark:text-dark-gray-800">
+            <img
+              src={product.images[0]}
+              alt={product.name}
+              className="size-[100px] rounded-lg object-cover"
+            />
+          </p>
+        </div>
+        <div>
+          <Label htmlFor="price">Price</Label>
+          <Input
+            id="price"
+            type="number"
+            placeholder="Price"
+            defaultValue={price.unit_amount / 100}
+          />
+        </div>
+        <Button>Edit Product</Button>
+      </div>
+    )
+  }
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10 p-4">
+    <section className="container grid items-center gap-6 p-4 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
-        {product ? <ProductSection product={product} /> : <AddStripeProduct />}
+        {product ? (
+          <ProductSection product={product} />
+        ) : (
+          <AddStripeProduct object={object} />
+        )}
       </div>
     </section>
   )
